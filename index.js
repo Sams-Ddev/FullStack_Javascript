2f530e06021455f7ba33f7460b55f1a372e1fe6e
Sams-Ddev <sams.alxstudent@gmail.com>
2023-12-25 10:02:13 -0800

    uploaded my index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+//Javascript fundamentals
+
+// This course teaches the concepts of Javascript as:
+// Variables, Dynamic typing, Primitive/Primary value types and 
+// Reference type 
+
+let name = Kwadjo;
+let age = 30;
+
+//Lets put thesse variables inside of object
+let person = {
+  name: 'Mosh'  //Key value pairs
+  age: 30
+};
+
+console.log(person);
