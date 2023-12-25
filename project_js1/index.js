e447e7cd24d8d2ec3ecd0aa821dbd7d5710b525c
Sams-Ddev <sams.alxstudent@gmail.com>
2023-12-25 13:00:49 -0800

    learning notations

--- project_js1/index.js
+++ project_js1/index.js
@@ -1,16 +1,22 @@
 //Javascript fundamentals
 
 // This course teaches the concepts of Javascript as:
-// Variables, Dynamic typing, Primitive/Primary value types and 
-// Reference type 
+// Variables, Dynamic typing, Primitive/Primary value types and
+// Reference type : String literal, Number literal, Boolean literal
 
 let name = Kwadjo;
 let age = 30;
 
 //Lets put thesse variables inside of object
 let person = {
-  name: 'Mosh'  //Key value pairs
-  age: 30
+  name: "Mosh", //Key value pairs
+  age: 30,
 };
 
-console.log(person);
+//Dot Notation
+person.name = "John";
+console.log(person.name);
+
+//Bracket Notation
+console["name"] = "Mary";
+console.log(person.name);
