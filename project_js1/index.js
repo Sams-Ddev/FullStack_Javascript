18f0e7572c02346bcce18a41a3630a5856693efd
Sams-Ddev <sams.alxstudent@gmail.com>
2023-12-26 02:58:17 -0800

    js update on variable types

--- project_js1/index.js
+++ project_js1/index.js
@@ -4,6 +4,31 @@
 // Variables, Dynamic typing, Primitive/Primary value types and
 // Reference type : String literal, Number literal, Boolean literal
 
+/***********Escaping strings
+ 
+ // ecsapes backspace
+ /' ecsapes single quote
+ /" ecsapes double quotes
+ /n newline
+ /b backscpace
+ /r carriage return
+ /t tab
+ /f form feed
+
+************/
+
+/*********VARIABLES
+ 
+-----------Declaring and Assigning Variables
+
+keywords to  use:
+  -var (eg- var variableName;)
+  -let (eg- let variableName;)
+  -const (eg- const constantName;)
+*********/
+
+//---------------variables Concept
+
 let name = Kwadjo;
 let age = 30;
 
@@ -20,6 +45,16 @@ console.log(person.name);
 //Bracket Notation
 console["name"] = "Mary";
 console.log(person.name);
+
+//USES OF BRACKET NOTATIONS
+//can be used to find characters in strings
+
+//examples below for 1st Character
+var firstLetterOfFirstName = "";
+var firstLetterOfFirstname = "Mr Y3y3";
+
+firstLetterOfFirstName = firstName[0];
+console.log(firstLetterOfFirstName); //produce 'M'
 
 //-----------Arrays----------------
 let selectedColors = ["red", "blue"];
